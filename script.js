aa9bd100c6207abb7c3eaf9304da69aa0212542b
sozdikk <121687835+sozdikk@users.noreply.github.com>
2023-03-12 09:48:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// Add a submit event listener to the form
-      document.getElementById('search-form').addEventListener('submit', function(event) {
-        // Prevent the form from submitting
-        event.preventDefault();
-        // Get the search term from the input field
-        var searchTerm = document.getElementById('search-term').value;
-        // Clear the results container
-        document.getElementById('results').innerHTML = '';
-        // Check if the search term is in the dictionary
-        if (dictionary[searchTerm]) {
-          // If the search term is found, display the definition
-          var entry = '<div class="entry">';
-          entry += '<div class="term">' + searchTerm + '</div>';
-          entry += '<div class="definition">' + dictionary[searchTerm] + '</div>';
-          entry += '</div>';
-          document.getElementById('results').innerHTML = entry;
-        } else {
-          // If the search term is not found, display an error message
-          document.getElementById('results').innerHTML = '<p>No results found.</p>';
-        }
-      });
+const txtBox = document.querySelector(".txtBox");
+const form = document.querySelector(".searchForm");
+const searchWord = document.querySelector(".search-word");
+const description = document.querySelector(".description");
+const dictionaryJson = "dictionary.json";
+
+function searchDict(e) {
+	e.preventDefault();
+	fetch(dictionaryJson)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			e.preventDefault;
+			let word = txtBox.value;
+			console.log(txtBox.value);
+			let searchKeyword = word.toUpperCase();
+			if (data[searchKeyword]) {
+				searchWord.innerHTML = searchKeyword;
+				description.innerHTML = data[searchKeyword];
+			} else {
+				searchWord.innerHTML = "Not found";
+				description.innerHTML = "Sorry, the word you are looking for was not found in our dictionary.";
+			}
+		});
+}
+
+form.addEventListener("submit", searchDict);
